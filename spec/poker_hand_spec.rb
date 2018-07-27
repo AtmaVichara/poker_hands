5fe8413d70af2926110de2c98bb6e7dae35219bd
Joe Jobes <jmrjobes@gmail.com>
2018-07-27 06:09:03 -0600

    Add initialization spec for PokerHand

---
+++ spec/poker_hand_spec.rb
@@ -0,0 +1,14 @@
+require "rspec"
+require "../lib/poker_hand"
+require "../lib/card"
+
+describe "Poker Hand Spec" do
+  describe "Attributes" do
+    it "should contain array of cards" do
+      card = double("card", value: "4", suite: "C")
+      poker_hand = PokerHand.new([card, card, card, card, card])
+      expect(poker_hand).to_not be_nil
+      expect(poker_hand.cards.length).to eq(5)
+    end
+  end
+end
