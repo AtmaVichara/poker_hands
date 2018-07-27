a5e173c3142ccc4f26b93c4ba9c1ee461d12761d
Joe Jobes <jmrjobes@gmail.com>
2018-07-27 09:53:59 -0600

    Add card class
    Add Card class with set_values method

--- lib/card.rb
+++ lib/card.rb
@@ -3,8 +3,13 @@ class Card
   attr_reader :value, :suite
 
   def initialize(attrs)
-    @value = attrs[0]
+    @value = set_values(attrs[0])
     @suite = attrs[1]
+  end
+
+  def set_values(value)
+    substitute = {"A" => "14", "K" => "13", "Q" => "12", "J" => "11", "T" => "10"}
+    substitute.has_key?(value) ? substitute[value] : value
   end
 
 end
